19ff7357025b1313b18d3bd6f2144580d42384f6
Marco <hictechsrl@gmail.com>
2020-12-05 10:28:25 +0100

    nuovi gruppi iphones

--- www/js/poscale.js
+++ www/js/poscale.js
@@ -255,4 +255,90 @@ function disallowOverscroll(){
       e.stopPropagation();
     });
   }
+
+
+function getPhoneInfo(val) {
+
+  switch( val ) {
+  
+            /* group_a */
+            case "iPhone13,4":                              return { name:"iPhone 12 Pro Max", group: "group_a"}
+            case "iPhone10,2", "iPhone10,5":                return { name:"iPhone 8 Plus", group: "group_a"}
+            case "iPhone9,2", "iPhone9,4":                  return { name:"iPhone 7 Plus", group: "group_a"}
+            case "iPhone8,2":                               return { name:"iPhone 6s Plus", group: "group_a"}
+            case "iPhone7,1":                               return { name:"iPhone 6 Plus", group: "group_a"}
+            case "iPhone12,5":                              return { name:"iPhone 11 Pro Max", group: "group_a"}
+            case "iPhone11,4", "iPhone11,6":                return { name:"iPhone XS Max", group: "group_a"}
+
+            /* group_b */
+            case "iPhone11,8":                              return { name:"iPhone XR", group: "group_b"}
+            case "iPhone12,1":                              return { name:"iPhone 11", group: "group_b"}
+
+    		/* group_c */
+            case "iPhone13,2":                              return { name:"iPhone 12", group: "group_c"}
+            case "iPhone13,3":                              return { name:"iPhone 12 Pro", group: "group_c"}
+
+
+            /* group_d */
+            case "iPhone12,3":                              return { name:"iPhone 11 Pro", group: "group_d"}
+            case "iPhone11,2":                              return { name:"iPhone XS", group: "group_d"}
+            case "iPhone10,3", "iPhone10,6":                return { name:"iPhone X", group: "group_d"}
+
+            /* group_e */
+            case "iPhone12,8":                              return { name:"iPhone SE (2nd generation)", group: "group_e"}
+            case "iPhone9,1", "iPhone9,3":                  return { name:"iPhone 7", group: "group_e"}
+            case "iPhone10,1", "iPhone10,4":                return { name:"iPhone 8", group: "group_e"}
+            case "iPhone7,2":                               return { name:"iPhone 6", group: "group_e"}
+            case "iPhone8,1":                               return { name:"iPhone 6s", group: "group_e"}
+            
+
+            /* group_f */
+            case "iPhone13,1":                              return { name:"iPhone 12 mini", group: "group_f"}
+
+
+            /* group_g */
+            case "iPhone5,1", "iPhone5,2":                  return { name:"iPhone 5", group: "group_g"}
+            case "iPhone5,3", "iPhone5,4":                  return { name:"iPhone 5c", group: "group_g"}
+            case "iPhone6,1", "iPhone6,2":                  return { name:"iPhone 5s", group: "group_g"}            
+            case "iPhone8,4":                               return { name:"iPhone SE", group: "group_g"}
+
+
+
+
+            /*
+            case "iPhone3,1", "iPhone3,2", "iPhone3,3":     return { name:"iPhone 4", group: "group_a"}
+            case "iPhone4,1":                               return { name:"iPhone 4s", group: "group_a"}
+
+
+            case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":return { name:"iPad 2", group: "group_a"}
+            case "iPad3,1", "iPad3,2", "iPad3,3":           return { name:"iPad (3rd generation)", group: "group_a"}
+            case "iPad3,4", "iPad3,5", "iPad3,6":           return { name:"iPad (4th generation)", group: "group_a"}
+            case "iPad6,11", "iPad6,12":                    return { name:"iPad (5th generation)", group: "group_a"}
+            case "iPad7,5", "iPad7,6":                      return { name:"iPad (6th generation)", group: "group_a"}
+            case "iPad7,11", "iPad7,12":                    return { name:"iPad (7th generation)", group: "group_a"}
+            case "iPad11,6", "iPad11,7":                    return { name:"iPad (8th generation)", group: "group_a"}
+            case "iPad4,1", "iPad4,2", "iPad4,3":           return { name:"iPad Air", group: "group_a"}
+            case "iPad5,3", "iPad5,4":                      return { name:"iPad Air 2", group: "group_a"}
+            case "iPad11,3", "iPad11,4":                    return { name:"iPad Air (3rd generation)", group: "group_a"}
+            case "iPad13,1", "iPad13,2":                    return { name:"iPad Air (4th generation)", group: "group_a"}
+            case "iPad2,5", "iPad2,6", "iPad2,7":           return { name:"iPad mini", group: "group_a"}
+            case "iPad4,4", "iPad4,5", "iPad4,6":           return { name:"iPad mini 2", group: "group_a"}
+            case "iPad4,7", "iPad4,8", "iPad4,9":           return { name:"iPad mini 3", group: "group_a"}
+            case "iPad5,1", "iPad5,2":                      return { name:"iPad mini 4", group: "group_a"}
+            case "iPad11,1", "iPad11,2":                    return { name:"iPad mini (5th generation)", group: "group_a"}
+            case "iPad6,3", "iPad6,4":                      return { name:"iPad Pro (9.7-inch)", group: "group_a"}
+            case "iPad7,3", "iPad7,4":                      return { name:"iPad Pro (10.5-inch)", group: "group_a"}
+            case "iPad8,1", "iPad8,2", "iPad8,3", "iPad8,4":return { name:"iPad Pro (11-inch) (1st generation)", group: "group_a"}
+            case "iPad8,9", "iPad8,10":                     return { name:"iPad Pro (11-inch) (2nd generation)", group: "group_a"}
+            case "iPad6,7", "iPad6,8":                      return { name:"iPad Pro (12.9-inch) (1st generation)", group: "group_a"}
+            case "iPad7,1", "iPad7,2":                      return { name:"iPad Pro (12.9-inch) (2nd generation)", group: "group_a"}
+            case "iPad8,5", "iPad8,6", "iPad8,7", "iPad8,8":return { name:"iPad Pro (12.9-inch) (3rd generation)", group: "group_a"}
+            case "iPad8,11", "iPad8,12":                    return { name:"iPad Pro (12.9-inch) (4th generation)", group: "group_a"}
+            */
+   
+  }
+
+}
+
+console.log(theTest("iPhone10,5"));
   
--- www/js/rotate.js
+++ www/js/rotate.js
@@ -10,7 +10,7 @@ function rotate($line,val){
 
     
     // group_a
-    // iphone 11, XR, 11 pro max, XS max
+    // 12 Pro Max, 8 Plus, 7 Plus, 6s Plus, 6 Plus, 11 Pro Max, XS Max
     if(current_group == "group_a"){
         var num = val * 100;
         if(num < 50)
@@ -22,7 +22,7 @@ function rotate($line,val){
 
 
     // group_b
-    // iphone 11 pro, X, XS
+    // 11, XR
     if(current_group == "group_b"){
         var num = val * 100;
         if(num < 50)
@@ -34,7 +34,7 @@ function rotate($line,val){
 
 
     // group_c
-    // iphone 8, 7
+    // 12, 12 Pro
     if(current_group == "group_c"){
         var num = val * 100;
         if(num < 50)
@@ -46,7 +46,7 @@ function rotate($line,val){
 
 
     // group_d
-    // iphone 8 plus, 7 plus, 6s plus, 6s
+    // 11 Pro, XS, X
     if(current_group == "group_d"){
         var num = val * 100;
         if(num < 50)
@@ -54,6 +54,38 @@ function rotate($line,val){
         else
             gain = 133
     }
+
+
+    // group_e
+    // SE 2nd generation, 6, 7, 8, 6s
+    if(current_group == "group_d"){
+        var num = val * 100;
+        if(num < 50)
+            gain = 145;
+        else
+            gain = 133
+    }
+
+    // group_f
+    // 12 mini
+    if(current_group == "group_d"){
+        var num = val * 100;
+        if(num < 50)
+            gain = 145;
+        else
+            gain = 133
+    }
+
+    // group_g
+    // 5, 5c, 5s, SE
+    if(current_group == "group_d"){
+        var num = val * 100;
+        if(num < 50)
+            gain = 145;
+        else
+            gain = 133
+    }
+
 
 
     
