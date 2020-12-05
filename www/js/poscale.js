a0d92ba327f3e547dca0402ec66d722093218ed0
Marco <hictechsrl@gmail.com>
2020-12-05 10:37:19 +0100

    nuovi gruppi iphone

--- www/js/poscale.js
+++ www/js/poscale.js
@@ -28,11 +28,7 @@ function onDeviceReady() {
     setModel($model, $group);
     disallowOverscroll();
 
-    // disattivo la statusbar
     StatusBar.hide();
-
-    // impedisco lo spegnimento del monitor
-    window.plugins.insomnia.keepAwake();
     
     /* bolla */
     setInterval(function(){
@@ -58,7 +54,7 @@ function onDeviceReady() {
         //con questo metto la lancetta a zero considerando y_initial
         y_avg = y_avg + config.y_initial + 0.08; // questo 0.08 è per azzerare una leggere discrepanza per cui quando la bolla è perfettamente centrata l'indicatore arancione sul perimetro non è perfettammente al centro della tacca dello zero
 
-        //$y.html(y_avg);
+        $y.html(y_avg);
         y_values = [];
         
         rotate($line,y_avg);
@@ -143,82 +139,14 @@ function bindClick(){
 */
 
 
-function setModel($elem,$group){
+function setModel($model,$group){
 
-    // setto la classe al body
-    if( device.model == "iPhone12,1" || device.model == "iPhone11,8" || device.model == "iPhone12,5" || device.model == "iPhone11,4" || device.model == "iPhone11,6" ){
-        $("body").addClass("group_a");
-        //$group.html("group_a");
-    }
-    if( device.model == "iPhone12,3" || device.model == "iPhone13" || device.model == "iPhone10,6" || device.model == "iPhone11,2"){
-        $("body").addClass("group_b");
-        //$group.html("group_b");
-    }
-    if( device.model == "iPhone10,1" || device.model == "iPhone10,4" || device.model == "iPhone9,1" || device.model == "iPhone9,3" ){
-        $("body").addClass("group_c");
-        //$group.html("group_c");
-    }
-    if( device.model == "iPhone10,2" || device.model == "iPhone10,5" || device.model == "iPhone9,2" || device.model == "iPhone9,4" || device.model == "iPhone8,2" || device.model == "iPhone8,1" ){
-        $("body").addClass("group_d");
-        //$group.html("group_d");
-    }
+    var iphone = getPhoneInfo(device.model);
+    $("body").addClass(iphone.group);
+    $group.html(iphone.group);
+    $model.html(iphone.name);
 
-    // printo il nome del modello
-    if(device.model == "iPhone7,2"){
-        $elem.html("6");
-    }
-
-    if(device.model == "iPhone8,1"){
-        $elem.html("6S");
-    }
-
-    if(device.model == "iPhone9,1" || device.model == "iPhone9,3"){
-        $elem.html("7");
-    }
-
-    if(device.model == "iPhone9,2" || device.model == "iPhone9,4"){
-        $elem.html("7 plus");
-    }
-
-    if(device.model == "iPhone10,4" || device.model == "iPhone10,1"){
-        $elem.html("8");
-    }
-
-    if(device.model == "iPhone10,5" || device.model == "iPhone10,2"){
-        $elem.html("8 plus");
-    }
-
-    if(device.model == "iPhone10,3" || device.model == "iPhone10,6"){
-        $elem.html("X");
-    }
-
-    if(device.model == "iPhone11,2"){
-        $elem.html("XS");
-    }
-
-    if(device.model == "iPhone11,4" || device.model == "iPhone11,6"){
-        $elem.html("XS max");
-    }
-
-    if(device.model == "iPhone11,8"){
-        $elem.html("XR");
-    }
-
-    if(device.model == "iPhone12,1"){
-        $elem.html("11");
-    }
-
-    if(device.model == "iPhone12,3"){
-        $elem.html("11 pro");
-    }
-
-    if(device.model == "iPhone12,5"){
-        $elem.html("11 pro max");
-    }
-
-    if(device.model == "iPhone12,8"){
-        $elem.html("SE");
-    }
+   
 }
 
 
@@ -259,4 +187,90 @@ function disallowOverscroll(){
       e.stopPropagation();
     });
   }
+
+
+function getPhoneInfo(val) {
+
+  switch( val ) {
+  
+            /* group_a */
+            case "iPhone13,4":                              return { name:"iPhone 12 Pro Max", group: "group_a"}
+            case "iPhone10,2", "iPhone10,5":                return { name:"iPhone 8 Plus", group: "group_a"}
+            case "iPhone9,2", "iPhone9,4":                  return { name:"iPhone 7 Plus", group: "group_a"}
+            case "iPhone8,2":                               return { name:"iPhone 6s Plus", group: "group_a"}
+            case "iPhone7,1":                               return { name:"iPhone 6 Plus", group: "group_a"}
+            case "iPhone12,5":                              return { name:"iPhone 11 Pro Max", group: "group_a"}
+            case "iPhone11,4", "iPhone11,6":                return { name:"iPhone XS Max", group: "group_a"}
+
+            /* group_b */
+            case "iPhone11,8":                              return { name:"iPhone XR", group: "group_b"}
+            case "iPhone12,1":                              return { name:"iPhone 11", group: "group_b"}
+
+    		/* group_c */
+            case "iPhone13,2":                              return { name:"iPhone 12", group: "group_c"}
+            case "iPhone13,3":                              return { name:"iPhone 12 Pro", group: "group_c"}
+
+
+            /* group_d */
+            case "iPhone12,3":                              return { name:"iPhone 11 Pro", group: "group_d"}
+            case "iPhone11,2":                              return { name:"iPhone XS", group: "group_d"}
+            case "iPhone10,3", "iPhone10,6":                return { name:"iPhone X", group: "group_d"}
+
+            /* group_e */
+            case "iPhone12,8":                              return { name:"iPhone SE (2nd generation)", group: "group_e"}
+            case "iPhone9,1", "iPhone9,3":                  return { name:"iPhone 7", group: "group_e"}
+            case "iPhone10,1", "iPhone10,4":                return { name:"iPhone 8", group: "group_e"}
+            case "iPhone7,2":                               return { name:"iPhone 6", group: "group_e"}
+            case "iPhone8,1":                               return { name:"iPhone 6s", group: "group_e"}
+            
+
+            /* group_f */
+            case "iPhone13,1":                              return { name:"iPhone 12 mini", group: "group_f"}
+
+
+            /* group_g */
+            case "iPhone5,1", "iPhone5,2":                  return { name:"iPhone 5", group: "group_g"}
+            case "iPhone5,3", "iPhone5,4":                  return { name:"iPhone 5c", group: "group_g"}
+            case "iPhone6,1", "iPhone6,2":                  return { name:"iPhone 5s", group: "group_g"}            
+            case "iPhone8,4":                               return { name:"iPhone SE", group: "group_g"}
+
+
+
+
+            /*
+            case "iPhone3,1", "iPhone3,2", "iPhone3,3":     return { name:"iPhone 4", group: "group_a"}
+            case "iPhone4,1":                               return { name:"iPhone 4s", group: "group_a"}
+
+
+            case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":return { name:"iPad 2", group: "group_a"}
+            case "iPad3,1", "iPad3,2", "iPad3,3":           return { name:"iPad (3rd generation)", group: "group_a"}
+            case "iPad3,4", "iPad3,5", "iPad3,6":           return { name:"iPad (4th generation)", group: "group_a"}
+            case "iPad6,11", "iPad6,12":                    return { name:"iPad (5th generation)", group: "group_a"}
+            case "iPad7,5", "iPad7,6":                      return { name:"iPad (6th generation)", group: "group_a"}
+            case "iPad7,11", "iPad7,12":                    return { name:"iPad (7th generation)", group: "group_a"}
+            case "iPad11,6", "iPad11,7":                    return { name:"iPad (8th generation)", group: "group_a"}
+            case "iPad4,1", "iPad4,2", "iPad4,3":           return { name:"iPad Air", group: "group_a"}
+            case "iPad5,3", "iPad5,4":                      return { name:"iPad Air 2", group: "group_a"}
+            case "iPad11,3", "iPad11,4":                    return { name:"iPad Air (3rd generation)", group: "group_a"}
+            case "iPad13,1", "iPad13,2":                    return { name:"iPad Air (4th generation)", group: "group_a"}
+            case "iPad2,5", "iPad2,6", "iPad2,7":           return { name:"iPad mini", group: "group_a"}
+            case "iPad4,4", "iPad4,5", "iPad4,6":           return { name:"iPad mini 2", group: "group_a"}
+            case "iPad4,7", "iPad4,8", "iPad4,9":           return { name:"iPad mini 3", group: "group_a"}
+            case "iPad5,1", "iPad5,2":                      return { name:"iPad mini 4", group: "group_a"}
+            case "iPad11,1", "iPad11,2":                    return { name:"iPad mini (5th generation)", group: "group_a"}
+            case "iPad6,3", "iPad6,4":                      return { name:"iPad Pro (9.7-inch)", group: "group_a"}
+            case "iPad7,3", "iPad7,4":                      return { name:"iPad Pro (10.5-inch)", group: "group_a"}
+            case "iPad8,1", "iPad8,2", "iPad8,3", "iPad8,4":return { name:"iPad Pro (11-inch) (1st generation)", group: "group_a"}
+            case "iPad8,9", "iPad8,10":                     return { name:"iPad Pro (11-inch) (2nd generation)", group: "group_a"}
+            case "iPad6,7", "iPad6,8":                      return { name:"iPad Pro (12.9-inch) (1st generation)", group: "group_a"}
+            case "iPad7,1", "iPad7,2":                      return { name:"iPad Pro (12.9-inch) (2nd generation)", group: "group_a"}
+            case "iPad8,5", "iPad8,6", "iPad8,7", "iPad8,8":return { name:"iPad Pro (12.9-inch) (3rd generation)", group: "group_a"}
+            case "iPad8,11", "iPad8,12":                    return { name:"iPad Pro (12.9-inch) (4th generation)", group: "group_a"}
+            */
+   
+  }
+
+}
+
+console.log(theTest("iPhone10,5"));
   
--- www/js/rotate.js
+++ www/js/rotate.js
@@ -5,74 +5,96 @@
 
 function rotate($line,val){
 
+    var gain;
     var current_group = $("body").attr("class");
-    var num = val * 100;
-    var ratio = 6.5901;
 
     
     // group_a
-    // iphone 11, XR, 11 pro max, XS max
+    // 12 Pro Max, 8 Plus, 7 Plus, 6s Plus, 6 Plus, 11 Pro Max, XS Max
     if(current_group == "group_a"){
-        // il rapporto num/grammi è 6,5901 calcolato come rapporto medio ottenuto dalla taratura
-        // ho individuato questi due scaloni che rettificano ulteriormente il rapporto
-        if(num <= 85)
-            ratio = 6.65;
-        if(num > 85)
-            ratio = 6.85;
-              
+        var num = val * 100;
+        if(num < 50)
+            gain = 145;
+        else
+            gain = 133
     }
+
 
 
     // group_b
-    // iphone 11 pro, X, XS
+    // 11, XR
     if(current_group == "group_b"){
-        
+        var num = val * 100;
+        if(num < 50)
+            gain = 145;
+        else
+            gain = 133
     }
+
 
 
     // group_c
-    // iphone 8, 7
+    // 12, 12 Pro
     if(current_group == "group_c"){
-        
+        var num = val * 100;
+        if(num < 50)
+            gain = 145;
+        else
+            gain = 133
     }
+
 
 
     // group_d
-    // iphone 8 plus, 7 plus, 6s plus, 6s
+    // 11 Pro, XS, X
     if(current_group == "group_d"){
-        
+        var num = val * 100;
+        if(num < 50)
+            gain = 145;
+        else
+            gain = 133
     }
+
+
+    // group_e
+    // SE 2nd generation, 6, 7, 8, 6s
+    if(current_group == "group_d"){
+        var num = val * 100;
+        if(num < 50)
+            gain = 145;
+        else
+            gain = 133
+    }
+
+    // group_f
+    // 12 mini
+    if(current_group == "group_d"){
+        var num = val * 100;
+        if(num < 50)
+            gain = 145;
+        else
+            gain = 133
+    }
+
+    // group_g
+    // 5, 5c, 5s, SE
+    if(current_group == "group_d"){
+        var num = val * 100;
+        if(num < 50)
+            gain = 145;
+        else
+            gain = 133
+    }
+
 
 
     
 
-    var deg_decrement = (num / ratio ) * 10; // ogni tacca da un grammo equivale a 10° in meno
-    var degrees = deg_decrement - 165;
-    var exact_value = (deg_decrement/10).toFixed(2)
-
-    // ruoto la lancetta
+    
+    
+    
+    
+    
+    var degrees = (val * gain) - 165;
     $line.css({'transform' : 'rotate('+ degrees +'deg)'});
-    // ruoto il div del peso esatto
-    $("#exact_value").css({'transform' : 'rotate('+ degrees * (-1) +'deg)'});
-    $("#exact_value").html( exact_value );
-    
-    
-
-    if(exact_value > -0.15 && exact_value < 0.15){
-        $(".exact_value").addClass("in_bolla");
-        $(".place_container").addClass("active");
-    }
-    else{
-        $(".exact_value").removeClass("in_bolla");
-        $(".place_container").removeClass("active");
-    }
-
-
-    // solo di visualizzazione per sviluppare
-    $("#y").html(num);
-    $("#deg").html(deg_decrement);
-    
-
-    
-    
 }
