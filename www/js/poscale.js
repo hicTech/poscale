536e24e49d1d64715f4f124e06eae8c30f611fac
Marco <hictechsrl@gmail.com>
2020-12-05 10:34:23 +0100

    m c

--- www/js/poscale.js
+++ www/js/poscale.js
@@ -139,82 +139,14 @@ function bindClick(){
 */
 
 
-function setModel($elem,$group){
+function setModel($model,$group){
 
-    // setto la classe al body
-    if( device.model == "iPhone12,1" || device.model == "iPhone11,8" || device.model == "iPhone12,5" || device.model == "iPhone11,4" || device.model == "iPhone11,6" ){
-        $("body").addClass("group_a");
-        $group.html("group_a");
-    }
-    if( device.model == "iPhone12,3" || device.model == "iPhone13" || device.model == "iPhone10,6" || device.model == "iPhone11,2"){
-        $("body").addClass("group_b");
-        $group.html("group_b");
-    }
-    if( device.model == "iPhone10,1" || device.model == "iPhone10,4" || device.model == "iPhone9,1" || device.model == "iPhone9,3" ){
-        $("body").addClass("group_c");
-        $group.html("group_c");
-    }
-    if( device.model == "iPhone10,2" || device.model == "iPhone10,5" || device.model == "iPhone9,2" || device.model == "iPhone9,4" || device.model == "iPhone8,2" || device.model == "iPhone8,1" ){
-        $("body").addClass("group_d");
-        $group.html("group_d");
-    }
+    var iphone = getPhoneInfo(device.model);
+    $("body").addClass(iphone.group);
+    $group.html(iphone.group);
+    $model.html(iphone.name);
 
-    // printo il nome del modello
-    if(device.model == "iPhone7,2"){
-        $elem.html("6");
-    }
-
-    if(device.model == "iPhone8,1"){
-        $elem.html("6S");
-    }
-
-    if(device.model == "iPhone9,1" || device.model == "iPhone9,3"){
-        $elem.html("7");
-    }
-
-    if(device.model == "iPhone9,2" || device.model == "iPhone9,4"){
-        $elem.html("7 plus");
-    }
-
-    if(device.model == "iPhone10,4" || device.model == "iPhone10,1"){
-        $elem.html("8");
-    }
-
-    if(device.model == "iPhone10,5" || device.model == "iPhone10,2"){
-        $elem.html("8 plus");
-    }
-
-    if(device.model == "iPhone10,3" || device.model == "iPhone10,6"){
-        $elem.html("X");
-    }
-
-    if(device.model == "iPhone11,2"){
-        $elem.html("XS");
-    }
-
-    if(device.model == "iPhone11,4" || device.model == "iPhone11,6"){
-        $elem.html("XS max");
-    }
-
-    if(device.model == "iPhone11,8"){
-        $elem.html("XR");
-    }
-
-    if(device.model == "iPhone12,1"){
-        $elem.html("11");
-    }
-
-    if(device.model == "iPhone12,3"){
-        $elem.html("11 pro");
-    }
-
-    if(device.model == "iPhone12,5"){
-        $elem.html("11 pro max");
-    }
-
-    if(device.model == "iPhone12,8"){
-        $elem.html("SE");
-    }
+   
 }
 
 
